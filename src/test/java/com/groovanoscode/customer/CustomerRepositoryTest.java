cead70f09a024d84f734f5e40e9565fea4022767
btchuemkom <tchuemkom@outlook.de>
2023-04-14 14:43:46 +0200

    DAO Mock Testing

---
+++ src/test/java/com/groovanoscode/customer/CustomerRepositoryTest.java
@@ -0,0 +1,98 @@
+package com.groovanoscode.customer;
+
+import com.groovanoscode.AbstractTestcontainers;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.ApplicationContext;
+
+import java.util.UUID;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CustomerRepositoryTest extends AbstractTestcontainers {
+
+    @Autowired
+    private CustomerRepository underTest;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        underTest.deleteAll(); // Make sure we have no data into our database
+        System.out.println(applicationContext.getBeanDefinitionCount());
+    }
+
+    @Test
+    void existsCustomerByEmail() {
+        // Given
+        String email = FAKER.internet().safeEmailAddress() + "_" + UUID.randomUUID();
+
+        Customer customer = new Customer(
+                FAKER.name().fullName(),
+                email,
+                20
+        );
+
+        underTest.save(customer);
+
+        // When
+        var actual = underTest.existsCustomerByEmail(email);
+
+        // Then
+        Assertions.assertThat(actual).isTrue();
+    }
+
+    @Test
+    void existsCustomerByEmailFailsWhenEmailNotPresent() {
+        // Given
+        String email = FAKER.internet().safeEmailAddress() + "_" + UUID.randomUUID();
+
+        // When
+        var actual = underTest.existsCustomerByEmail(email);
+
+        // Then
+        Assertions.assertThat(actual).isFalse();
+    }
+
+    @Test
+    void existsCustomerById() {
+        // Given
+        String email = FAKER.internet().safeEmailAddress() + "_" + UUID.randomUUID();
+
+        Customer customer = new Customer(
+                FAKER.name().fullName(),
+                email,
+                20
+        );
+        underTest.save(customer);
+
+        int id = underTest.findAll().stream()
+                .filter(c -> c.getEmail().equals(email))
+                .map(customer1 -> customer1.getId())
+                .findFirst()
+                .orElseThrow();
+
+        // When
+        var actual = underTest.existsCustomerById(id);
+
+        // Then
+        Assertions.assertThat(actual).isTrue();
+    }
+
+    @Test
+    void existsCustomerByIdFailsWhenIdNotPresent() {
+        // Given
+        int id = -1;
+
+        // When
+        var actual = underTest.existsCustomerById(id);
+
+        // Then
+        Assertions.assertThat(actual).isFalse();
+    }
+}
