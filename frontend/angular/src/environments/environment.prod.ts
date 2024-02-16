a58cdc367c14572c7d009a1fdfc9e8ede53fed88
btchuemkom <tchuemkom@outlook.de>
2024-02-16 10:47:49 +0100

    Angular prod environment config Rev1

--- frontend/angular/src/environments/environment.prod.ts
+++ frontend/angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   api: {
-    baseUrl: 'https://customer-api.groovanoscode.dev',
+    baseUrl: 'https://customer-api.groovanoscode.com',
     authUrl: 'api/v1/auth/login',
     customerUrl: 'api/v1/customers'
   }
